7ba148188db770e6a6eaf2c1ef092f34e68cc4eb
jieun <jelee@ex-em.com>
2020-04-21 11:20:36 +0900

    create component

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ChartTest/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import ChartTest from './components/ChartTest.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    ChartTest,
   }
 }
 </script>
---
+++ src/components/ChartTest.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="chart">
+        <div id="main" ref="main" style="height: 100%"></div>
+    </div>
+</template>
+<script>
+    import echarts from 'echarts';
+
+    export default {
+        name: 'lineChart',
+        data() {
+            return {
+
+            };
+        },
+        mounted() {
+            const myChart = echarts.init(this.$refs.main);
+
+            myChart.setOption({
+                title: {
+                    text: 'ECharts entry example'
+                },
+                tooltip: {},
+                xAxis: {
+                    data: ['shirt', 'cardign', 'chiffon shirt', 'pants', 'heels', 'socks']
+                },
+                yAxis: {},
+                series: [{
+                    name: 'sales',
+                    type: 'bar',
+                    data: [5, 20, 36, 10, 10, 20]
+                }]
+            });
+        },
+    }
+
+</script>
+<style>
+    .chart{
+        width: 100%;
+        height: 300px;
+    }
+
+</style>
